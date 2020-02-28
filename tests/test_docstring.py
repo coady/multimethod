8d3dec09d8aac78cd4d4360bb50977a9960c2906
Bence Bagi <bagi.bence94@gmail.com>
2020-02-28 14:30:46 +0100

    Test if docstring of multimethod contains the docstring of its registered functions

---
+++ tests/test_docstring.py
@@ -0,0 +1,24 @@
+from multimethod import multimethod
+
+
+@multimethod
+def foo(bar: int):
+    """
+    Argument is an integer
+    """
+    pass
+
+@multimethod
+def foo(bar: str):
+    """
+    Argument is a string
+    """
+    pass
+
+
+def test_docstring():
+    """
+    Test if multimethod collects its children's docstrings
+    """
+    assert "Argument is an integer" in foo.__doc__
+    assert "Argument is a string" in foo.__doc__
